c7c9a83970d228300de7bba1cf3fc31beec3d7ba
Chance Reichenberg <chance.reichenberg@gmail.com>
2015-03-28 19:54:32 -0400

    Algorithm Analysis project Initial Commit

---
+++ AlgorithmAnalysis/AlgorithmAnalysisDriver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AlgorithmAnalysis
+{
+
+    /// <summary>
+    /// Class to drive algorithm analysis
+    /// </summary>
+    class AlgorithmAnalysisDriver
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int n = 10;
+            Random rand = new Random();
+
+            //Add n random numbers into list
+            for(int count = 0; count < n; count++)
+            {
+                list.Add(rand.Next());
+                Console.WriteLine(list[count]);
+            }
+
+            //Use sink sort to sort list
+            Sorts.SinkSort(ref list);
+            Console.WriteLine('\n');
+
+            for (int count = 0; count < n; count++ )
+            {
+                Console.WriteLine(list[count]);
+            }
+                Console.ReadLine();
+
+        }
+    }
+}
---
+++ AlgorithmAnalysis/Sorts.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAnalysis
+{
+    /// <summary>
+    /// Class to hold various sorting algorithms
+    /// </summary>
+    public static class Sorts
+    {
+        /// <summary>
+        /// Sink sort method
+        /// </summary>
+        /// <param name="list">List to sort</param>
+        public static void SinkSort(ref List<int> list)
+        {
+            bool sorted = false;
+            int pass = 0;
+
+            //Sort until sorted or enough  passes completed
+            while(!sorted && (pass < list.Count))
+            {
+                pass++;
+                sorted = true;
+
+                for(int i = 0; i < list.Count - pass; i++)
+                {
+                    //If not sorted, swap
+                    if(list[i] > list[i + 1])
+                    {
+                        Swap(ref list, i, i + 1);
+                        sorted = false;
+
+                    }
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Swaps items in a list
+        /// </summary>
+        /// <param name="list">List to have items swapped</param>
+        /// <param name="n">index of the first item</param>
+        /// <param name="m">index of the second item </param>
+        public static void Swap(ref List<int> list, int n, int m)
+        {
+            int temp = list[n];
+            list[n] = list[m];
+            list[m] = temp;
+            
+        }
+    }
+}
